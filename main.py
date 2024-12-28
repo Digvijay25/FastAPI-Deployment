7e17390a3a29f811b2aba4a44cdb33772d8c4b51
Digvijay Singh <digvijay2560@gmail.com>
2024-12-28 23:44:32 +0000

    model creation done

---
+++ main.py
@@ -0,0 +1,30 @@
+import pickle
+import pandas as pd 
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+
+df = pd.read_csv('BankNote_Authentication.csv')
+
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+classifier = RandomForestClassifier()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+score = accuracy_score(y_test, y_pred)
+
+pickle_out = open('classifier.pkl', 'wb')
+pickle.dump( classifier, pickle_out)
+pickle_out.close()
+
+
+
+
+
+
